b6c7d26cf011be0cc6a0fb170de15c459da66668
ifreeky <ifreeky@outlook.com>
2024-12-15 21:11:20 +0800

    add

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,31 @@
 return {
-  { "ellisonleao/gruvbox.nvim", "shaunsingh/solarized.nvim" },
-  -- Configure LazyVim to load gruvbox
+  -- 主题插件配置
+  {
+    "ellisonleao/gruvbox.nvim",
+    lazy = true, -- 延迟加载
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true, -- 延迟加载
+  },
+  {
+    "navarasu/onedark.nvim",
+    lazy = true, -- 延迟加载
+  },
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false, -- 确保 cyberdream 立即加载
+    priority = 1000, -- 确保 cyberdream 在其他主题之前加载
+    opts = {
+      transparent = true,
+    },
+  },
+
+  -- 配置 LazyVim 使用 cyberdream 作为默认主题
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "solarized",
+      colorscheme = "cyberdream", -- 设置默认配色
     },
   },
 }

4497b5628693a6bdfeac24ee33eb7565e1525239
dangteng <dangteng@micunbanfan.com>
2026-03-20 10:14:25 +0800

    feat(nvim): 调整剪贴板映射与主题配置

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,12 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --
+local function copy_current_line_to_system_clipboard()
+  local row = vim.api.nvim_win_get_cursor(0)[1]
+  local line = vim.api.nvim_buf_get_lines(0, row - 1, row, false)[1] or ""
+  vim.fn.setreg("+", line)
+end
+
 -- 删除不复制到寄存器
 vim.keymap.set({ "n", "v" }, "d", [["_d]], { desc = "Delete without yank" })
 vim.keymap.set({ "n", "v" }, "D", [["_D]], { desc = "Delete until end without yank" })
@@ -19,3 +25,8 @@ vim.keymap.set({ "n", "v", "o" }, "H", "^", { desc = "Go line start" })
 vim.keymap.set({ "n", "v", "o" }, "L", "$", { desc = "Go line end" })
 -- 插入模式下，快速 jj 退出到普通模式
 vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = true })
+
+-- Explicit macOS copy mappings for GUIs/frontends that forward Command keys.
+vim.keymap.set("v", "<D-c>", '"+y', { desc = "Copy selection to system clipboard" })
+vim.keymap.set("n", "<D-c>", '"+yy', { desc = "Copy line to system clipboard" })
+vim.keymap.set("i", "<D-c>", copy_current_line_to_system_clipboard, { desc = "Copy current line to system clipboard" })
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,26 +8,11 @@ return {
     "Tsuzat/NeoSolarized.nvim",
     lazy = false, -- 延迟加载
     priority = 1000,
-    config = function()
-      vim.cmd([[ colorscheme NeoSolarized ]])
-    end,
   },
   {
     "navarasu/onedark.nvim",
     lazy = true, -- 延迟加载
   },
-  {
-    "scottmckendry/cyberdream.nvim",
-    lazy = true, -- 确保 cyberdream 立即加载
-    priority = 1000, -- 确保 cyberdream 在其他主题之前加载
-    opts = {
-      transparent = true,
-      borderless_telescope = true,
-      terminal_colors = true,
-    },
-  },
-
-  -- 配置 LazyVim 使用 cyberdream 作为默认主题
   {
     "LazyVim/LazyVim",
     opts = {

b6f4693cd9727789e11c3fa5b6ef32adb6a0f3b9
dangteng <dangteng@micunbanfan.com>
2026-03-21 18:19:15 +0800

    refactor(nvim): regroup custom keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,7 +8,7 @@ local function copy_current_line_to_system_clipboard()
   vim.fn.setreg("+", line)
 end
 
--- 删除不复制到寄存器
+-- Editing without yank
 vim.keymap.set({ "n", "v" }, "d", [["_d]], { desc = "Delete without yank" })
 vim.keymap.set({ "n", "v" }, "D", [["_D]], { desc = "Delete until end without yank" })
 vim.keymap.set({ "n", "v" }, "c", [["_c]], { desc = "Change without yank" })
@@ -16,17 +16,16 @@ vim.keymap.set({ "n", "v" }, "C", [["_C]], { desc = "Change until end without ya
 vim.keymap.set({ "n", "v" }, "x", [["_x]], { desc = "Delete char without yank" })
 vim.keymap.set({ "n", "v" }, "X", [["_X]], { desc = "Delete char backward without yank" })
 
+-- Buffer navigation
 vim.keymap.set({ "n", "v" }, "E", "<cmd>bnext<CR>", { desc = "Next Buffer" })
 vim.keymap.set({ "n", "v" }, "R", "<cmd>bprevious<CR>", { desc = "Prev Buffer" })
 
--- H = 移动到行首
+-- Line movement and escape
 vim.keymap.set({ "n", "v", "o" }, "H", "^", { desc = "Go line start" })
--- L = 移动到行末
 vim.keymap.set({ "n", "v", "o" }, "L", "$", { desc = "Go line end" })
--- 插入模式下，快速 jj 退出到普通模式
 vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = true })
 
--- Explicit macOS copy mappings for GUIs/frontends that forward Command keys.
+-- macOS clipboard integration
 vim.keymap.set("v", "<D-c>", '"+y', { desc = "Copy selection to system clipboard" })
 vim.keymap.set("n", "<D-c>", '"+yy', { desc = "Copy line to system clipboard" })
 vim.keymap.set("i", "<D-c>", copy_current_line_to_system_clipboard, { desc = "Copy current line to system clipboard" })
